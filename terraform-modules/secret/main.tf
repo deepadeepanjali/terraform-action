96c7c47311174fd058f8f1b7db3613eb5f072f0a
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-01 20:00:01 +0530

    Update and rename 2.txt to main.tf

---
+++ terraform-modules/secret/main.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  features {}
+}
+
+# Fetch keyvault details by keyvaut names
+data "azurerm_key_vault" "keyvault" {
+  name                = var.keyvault                 # Use each Key Vault name
+  resource_group_name = var.resourcegroup   # Provide the resource group name where the Key Vaults exist
+}
+
+resource "azurerm_key_vault_secret" "secret" {
+  for_each = var.bot_configurations
+
+  name         = "secret-${var.foldername}-${each.value.botname}"
+  value        =  "123"
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
+
+
+resource "azurerm_role_assignment" "secret_officer_role" {
+  for_each = var.bot_configurations
+
+  # Assign the Secret Officer role to the Azure AD group for each secret
+  principal_id       = each.value.ad_group_objectid
+  role_definition_name = "Key Vault Secrets Officer"  # Secret Officer role grants create, update, and delete permissions on secrets
+  scope              = "${data.azurerm_key_vault.keyvault.id}/secrets/${azurerm_key_vault_secret.secret[each.key].name}"  # Assign to specific secret
+}
